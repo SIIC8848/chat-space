799c002fd171e178fc3c9e89e9b5a753572223d7
SIIC8848 <gx9901dx.seiichiro@gmail.com>
2020-01-07 11:17:41 +0900

    delete space message.js line59

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -56,7 +56,7 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.messages').append(html);      
+      $('.messages').append(html);
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
       $('form')[0].reset();
      })
